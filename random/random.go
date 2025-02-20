63ba3ea1ee4548f1f50846bbedcfd459a838c071
sashithaf16 <sashithaf16.tech@gmail.com>
2025-02-20 21:19:46 +0530

    updated random

--- random/random.go
+++ random/random.go
@@ -5,5 +5,5 @@ import "github.com/google/uuid"
 // RandomNumber returns a random number.
 func RandomNumber() string {
 	str := uuid.New().String()
-	return str + "HELLO"
+	return str + "HELLO" + "UPDATED"
 }
